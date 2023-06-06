0bf21fa017afee1c761a0711813a04c74e5c295a
bilmoleque2 <gabriel.bil8@gmail.com>
2023-06-05 21:19:25 -0300

    Bearer token no swagger

--- Program.cs
+++ Program.cs
@@ -4,38 +4,80 @@ using Microsoft.EntityFrameworkCore;
 using OwListy.Data;
 using System.Text;
 using OwListy;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services
-.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(x =>
-{
-    x.RequireHttpsMetadata = false;
-    x.SaveToken = true;
-    x.TokenValidationParameters = new TokenValidationParameters
+    .AddAuthentication(x =>
     {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Settings.Token)),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ClockSkew = TimeSpan.Zero
-    };
-});
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Settings.Token)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
-builder.Services.AddCors(x => x.AddPolicy("corspolicy", build =>
-{
-    build.WithOrigins("").AllowAnyMethod().AllowAnyHeader();
-}));
+builder.Services.AddCors(
+    x =>
+        x.AddPolicy(
+            "corspolicy",
+            build =>
+            {
+                build.WithOrigins("").AllowAnyMethod().AllowAnyHeader();
+            }
+        )
+);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Minha Web API", Version = "v1" });
+
+    // Adicione essa linha para incluir o cabeçalho de autorização
+    c.AddSecurityDefinition(
+        "Bearer",
+        new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "Insira o token JWT no formato 'Bearer {token}'",
+            Name = "Authorization"
+        }
+    );
+
+    // Adicione essa linha para exigir autenticação globalmente
+    c.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        }
+    );
+});
 
 builder.Services.AddDbContext<OwListyDbContext>(options =>
 {
